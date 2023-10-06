51b618404a39b0f31566d7d770b022e7fbdea2b7
Kobe <dev.skyachieve91@gmail.com>
2023-10-07 08:15:10 +0900

    :sparkles: Feat: Custom UIView Class 생성.
    Custom UIView Class 인 GFContainerView 생성 및 내부 구현.

---
+++ GithubFollower/GithubFollower/View/GFContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  GFContainerView.swift
+//  GithubFollower
+//
+//  Created by Minseong Kang on 2023/10/07.
+//
+
+import UIKit
+
+class GFContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = BorderColor.white.value
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
