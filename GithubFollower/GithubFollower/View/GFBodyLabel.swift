ed8e21efd522f33d925dfbacad56f3ce5992b654
Kobe <dev.skyachieve91@gmail.com>
2023-10-06 19:17:31 +0900

    :recycle: Refactor: GFBodyLabel 내부 구현.
    Custom Label Class인 GFBodyLabel을 내부 구현하였습니다.

--- GithubFollower/GithubFollower/View/GFBodyLabel.swift
+++ GithubFollower/GithubFollower/View/GFBodyLabel.swift
@@ -8,5 +8,29 @@
 import UIKit
 
 final class GFBodyLabel: UILabel {
-
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        
+        self.textAlignment = textAlignment
+        
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
