a67b18e3a325471d332062b0d56a8916870fc082
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 18:41:48 +0900

    :sparkles: Feat: URLSessionable Protocol 구현.
    URLSessionable 이라는 이름으로 Protocol을 구현하였으며 이 프로토콜을 채택시 필수적으로 dataTask 메서드를 구현하도록 내부 코드를 구성하였습니다. 그리고 typealias를 활용하여 DataTaskCompletionHandler 라는 별칭을 만들어 활용하였습니다.

--- GithubFollower/GithubFollower/Model/Protocol/URLSessionProtocol/Interface/URLSessionable.swift
+++ GithubFollower/GithubFollower/Model/Protocol/URLSessionProtocol/Interface/URLSessionable.swift
@@ -4,5 +4,10 @@
 //
 //  Created by Minseong Kang on 2023/10/08.
 //
-
 import Foundation
+
+typealias DataTaskCompletionHandler = @Sendable (Data?, URLResponse?, Error?) -> Void
+
+protocol URLSessionable {
+    func dataTask(with url: URL, completionHandler: @escaping DataTaskCompletionHandler) -> URLSessionDataTask
+}
