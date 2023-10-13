ced358f81c55ae4f303ad9ed441fc2159a57167e
Kobe <dev.skyachieve91@gmail.com>
2023-10-14 07:07:25 +0900

    :recycle: Refactor: ErrorMessage 적용.
    String으로 작성되어있던 메시지를 ErrorMessage 열거형을 활용, 교체 및 수정하였습니다.

--- GithubFollower/GithubFollower/Model/Protocol/NetworkProtocol/Implementation/NetworkManagerProtocol.swift
+++ GithubFollower/GithubFollower/Model/Protocol/NetworkProtocol/Implementation/NetworkManagerProtocol.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import OSLog
 
 struct NetworkManagerProtocol: NetworkManageable {
     var urlSession: URLSessionable
@@ -14,7 +15,7 @@ struct NetworkManagerProtocol: NetworkManageable {
         self.urlSession = urlSession
     }
     
-    func getFollower(for username: String, perPage: Int, page: Int, completionHandler: @escaping ([Follower]?, String?) -> Void) {
+    func getFollower(for username: String, perPage: Int, page: Int, completionHandler: @escaping ([Follower]?, ErrorMessage?) -> Void) {
         
         var components = URLComponents()
         components.scheme = Components.githubScheme.localized
@@ -30,18 +31,18 @@ struct NetworkManagerProtocol: NetworkManageable {
         ]
         
         guard let url = components.url else {
-            completionHandler(nil, "리퀘스트 URL 에러입니다.")
+            completionHandler(nil, ErrorMessage.invalidRequest)
             return
         }
         
         let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
             guard error == nil else {
-                completionHandler(nil, "에러가 발생했습니다.")
+                completionHandler(nil, ErrorMessage.unableRequest)
                 return
             }
             
             guard let response = response as? HTTPURLResponse else {
-                completionHandler(nil, "HTTP Response 에러가 발생했습니다.")
+                completionHandler(nil, ErrorMessage.invalidServer)
                 return
             }
             
@@ -49,18 +50,20 @@ struct NetworkManagerProtocol: NetworkManageable {
             
             if #available(iOS 16.0, *) {
                 guard successRange.contains(response.statusCode) else {
-                    completionHandler(nil, "\(response.statusCode) 에러 입니다.")
+                    Logger().error("\(response.statusCode) Error")
+                    completionHandler(nil, ErrorMessage.invalidResponse)
                     return
                 }
             } else {
                 guard successRange ~= response.statusCode else {
-                    completionHandler(nil, "\(response.statusCode) 에러 입니다.")
+                    Logger().error("\(response.statusCode) Error")
+                    completionHandler(nil, ErrorMessage.invalidResponse)
                     return
                 }
             }
             
             guard let data = data else {
-                completionHandler(nil, "데이터가 존재하지 않습니다. 다시 시도해주세요.")
+                completionHandler(nil, ErrorMessage.invalidResponse)
                 return
             }
             
@@ -69,7 +72,7 @@ struct NetworkManagerProtocol: NetworkManageable {
                 let followerListData = try decoder.decodeJSON(type: [Follower].self, data: data)
                 completionHandler(followerListData, nil)
             } catch {
-                completionHandler(nil, "데이터가 존재하지 않습니다. 다시 시도해주세요.")
+                completionHandler(nil, ErrorMessage.invalidServer)
             }
         }
         dataTask.resume()
