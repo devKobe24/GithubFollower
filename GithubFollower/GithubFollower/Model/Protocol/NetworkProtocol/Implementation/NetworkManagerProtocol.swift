3711130d7602a6e07a6fb2284062d458ef37fd92
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 18:36:26 +0900

    :sparkles: Feat: NetworkManagerProtocol 구조체 구현.
    NetworkManageable Protocol을 채택하는 구현부인 NetworkManagerProtocol을 구조체로 구현하였습니다.

--- GithubFollower/GithubFollower/Model/Protocol/NetworkProtocol/Implementation/NetworkManagerProtocol.swift
+++ GithubFollower/GithubFollower/Model/Protocol/NetworkProtocol/Implementation/NetworkManagerProtocol.swift
@@ -6,3 +6,74 @@
 //
 
 import Foundation
+
+struct NetworkManagerProtocol: NetworkManageable {
+    var urlSession: URLSessionable
+    
+    init(urlSession: URLSessionable = URLSession.shared) {
+        self.urlSession = urlSession
+    }
+    
+    func getFollower(for username: String, perPage: Int, page: Int, completionHandler: @escaping ([Follower]?, String?) -> Void) {
+        
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.github.com"
+        components.path = "/users/\(username)/followers"
+        
+        let perPage = URLQueryItem(name: "per_page", value: "\(perPage)")
+        let page = URLQueryItem(name: "page", value: "\(page)")
+        
+        components.queryItems = [
+            perPage,
+            page
+        ]
+        
+        guard let url = components.url else {
+            completionHandler(nil, "리퀘스트 URL 에러입니다.")
+            return
+        }
+        
+//        let requestURL = URLRequest(url: url)
+        
+        let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+            guard error == nil else {
+                completionHandler(nil, "에러가 발생했습니다.")
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completionHandler(nil, "HTTP Response 에러가 발생했습니다.")
+                return
+            }
+            
+            let successRange = (200...299)
+            
+            if #available(iOS 16.0, *) {
+                guard successRange.contains(response.statusCode) else {
+                    completionHandler(nil, "\(response.statusCode) 에러 입니다.")
+                    return
+                }
+            } else {
+                guard successRange ~= response.statusCode else {
+                    completionHandler(nil, "\(response.statusCode) 에러 입니다.")
+                    return
+                }
+            }
+            
+            guard let data = data else {
+                completionHandler(nil, "데이터가 존재하지 않습니다. 다시 시도해주세요.")
+                return
+            }
+            
+            do {
+                let decoder = JSONDecodeProtocol()
+                let followerListData = try decoder.decodeJSON(type: [Follower].self, data: data)
+                completionHandler(followerListData, nil)
+            } catch {
+                completionHandler(nil, "데이터가 존재하지 않습니다. 다시 시도해주세요.")
+            }
+        }
+        dataTask.resume()
+    }
+}
