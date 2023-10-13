0b39f7605da2c3aad8717c85a3a88683c5fdafe8
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 22:26:17 +0900

    :sparkles: Feat: Github API에 활용되는 URLQueryItem name 열거형 생성.
    Github API에서 URLQueryItem의 name으로 활용되는 키워드를 열거형으로 생성하였습니다.

--- GithubFollower/GithubFollower/Model/Enum/QueryItem.swift
+++ GithubFollower/GithubFollower/Model/Enum/QueryItem.swift
@@ -5,4 +5,16 @@
 //  Created by Minseong Kang on 2023/10/13.
 //
 
-import Foundation
+enum QueryItem {
+    case perPage
+    case page
+    
+    var name: String {
+        switch self {
+        case .perPage:
+            return "per_page"
+        case .page:
+            return "page"
+        }
+    }
+}
