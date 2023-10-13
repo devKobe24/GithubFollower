2bab8b544469586f0f59343728e8633c8c76b31e
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 18:26:32 +0900

    :sparkles: Feat: Network Unit Test 작성.

--- GithubFollower/GFNetworkTests/GFNetworkTests.swift
+++ GithubFollower/GFNetworkTests/GFNetworkTests.swift
@@ -6,30 +6,92 @@
 //
 
 import XCTest
+@testable import GithubFollower
 
 final class GFNetworkTests: XCTestCase {
+    var sut: NetworkManagerProtocol!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = NetworkManagerProtocol()
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_올바른아이디를_URL에_요청시_데이터가_들어오는지() {
+        // given
+        let url = URL(string: "https://api.github.com/users/devKobe24/followers?per_page=1&page=1")!
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        // when
+        sut.urlSession.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            let decoder = JSONDecodeProtocol()
+            
+            do {
+                guard let followersData = try decoder.decodeJSON(type: [Follower].self, data: data).first else { return }
+                // then
+                XCTAssertNotNil(followersData)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_돌아오는_첫번째_login_데이터가_expectation과_같은_값을_내어주는지() {
+        // given
+        let url = URL(string: "https://api.github.com/users/devKobe24/followers?per_page=1&page=1")!
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)
+        
+        // when
+        sut.urlSession.dataTask(with: url) { data, response, error in
+            guard let data = data else {
+                return
+            }
+            let decoder = JSONDecodeProtocol()
+            
+            do {
+                guard let followersData = try decoder.decodeJSON(type: [Follower].self, data: data).first else { return }
+                let followerLoginData = XCTestExpectation(description: followersData.login)
+                let expectation = XCTestExpectation(description: "MaryJo-github")
+                // then
+                XCTAssertEqual(followerLoginData, expectation)
+            } catch {
+                print(error)
+            }
+        }.resume()
     }
-
+    
+    func test_URL_요청시_상태코드_200번이_들어오는지() {
+        // given
+        let expectation = 200
+        let url = URL(string: "https://api.github.com/users/devKobe24/followers?per_page=1&page=1")!
+        
+        // when
+        sut.urlSession.dataTask(with: url) { (data, response, error) in
+            guard let response = response as? HTTPURLResponse else {
+                return
+            }
+            
+            let statusCode = response.statusCode
+            
+            // then
+            XCTAssertEqual(statusCode, expectation)
+            
+        }.resume()
+    }
+    
+    func test_URL_요청시_에러가_nil이_들어오는지() {
+        // given
+        let url = URL(string: "https://api.github.com/users/devKobe24/followers?per_page=1&page=1")!
+        // when
+        sut.urlSession.dataTask(with: url) { (data, response, error) in
+            // then
+            XCTAssertNil(error)
+        }.resume()
+    }
 }
