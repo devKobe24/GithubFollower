0f9f99dda68407d5e3548c71a7c1743c5656c180
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 18:34:20 +0900

    :sparkles: Feat: JSONDecodable protocol 작성 및 구현.
    JSONDecodable 이라는 이름으로 protocol을 작성하였으며 이 프로토콜을 채택시 필수적으로 구현해야하는 메서드로 decodeJSON을 구현하도록 내부 코드를 구성하였습니다.

--- GithubFollower/GithubFollower/Model/Protocol/DecodeProtocol/Interface/JSONDecodable.swift
+++ GithubFollower/GithubFollower/Model/Protocol/DecodeProtocol/Interface/JSONDecodable.swift
@@ -4,5 +4,8 @@
 //
 //  Created by Minseong Kang on 2023/10/08.
 //
-
 import Foundation
+
+protocol JSONDecodable {
+    func decodeJSON<Value>(type: Value.Type, data: Data) throws -> Value where Value : Decodable
+}
