2d4a98bd381bd334f9e0db231bdfa698386c0352
Kobe <dev.skyachieve91@gmail.com>
2023-10-13 18:28:59 +0900

    :sparkles: Feat: DecodeError 열거형 생성 및 구현.
    Error 타입의 DecodeError 열거형을 생성, 구현하였습니다.

--- GithubFollower/GithubFollower/Model/Error/DecodeError.swift
+++ GithubFollower/GithubFollower/Model/Error/DecodeError.swift
@@ -5,4 +5,13 @@
 //  Created by Minseong Kang on 2023/10/08.
 //
 
-import Foundation
+enum DecodeError: Error {
+    case failedDecode
+    
+    var description: String {
+        switch self {
+        case .failedDecode:
+            return "Failed decode data"
+        }
+    }
+}
