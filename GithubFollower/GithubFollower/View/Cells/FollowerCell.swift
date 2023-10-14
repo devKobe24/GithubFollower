28de3cb55b58eccd9f6976a1641f0aa1f9837b82
Minseong Kang <dev.skyachieve91@gmail.com>
2023-10-14 12:36:29 +0900

    :recycle: Refactor: 커스텀 UICollectionViewCell 기본 구현.
    필수적인 UI 기본 구현을 위한 코드를 작성하였습니다.

--- GithubFollower/GithubFollower/View/Cells/FollowerCell.swift
+++ GithubFollower/GithubFollower/View/Cells/FollowerCell.swift
@@ -8,5 +8,60 @@
 import UIKit
 
 final class FollowerCell: UICollectionViewCell, Reusable {
+    let placeHolderImage: UIImage
     
+    lazy var avatarImageView: GFAvatarImageView = {
+        var avatarImageView = GFAvatarImageView(placeHolderImage: placeHolderImage)
+        
+        return avatarImageView
+    }()
+    
+    let usernameLabel: GFTitleLabel = GFTitleLabel(
+        textAlignment: .center,
+        fontSize: 16
+    )
+    
+    init(placeHolderImage: UIImage) {
+        self.placeHolderImage = placeHolderImage
+        super.init(frame: .zero)
+        
+        configureAvatarImageView()
+        configureUsernameLabel()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension FollowerCell {
+    func setup(follower: Follower) {
+        usernameLabel.text = follower.login
+    }
+}
+
+extension FollowerCell {
+    private func configureAvatarImageView() {
+        let padding: CGFloat = 8
+        
+        addSubview(avatarImageView)
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor, multiplier: 1)
+        ])
+    }
+    
+    private func configureUsernameLabel() {
+        let padding: CGFloat = 8
+        
+        addSubview(usernameLabel)
+        
+        NSLayoutConstraint.activate([
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.widthAnchor.constraint(equalTo: avatarImageView.widthAnchor, multiplier: 1),
+        ])
+    }
 }
