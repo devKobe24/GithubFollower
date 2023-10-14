7abdbceaae1f65594707038f1b1b7b7bc1df0275
Minseong Kang <dev.skyachieve91@gmail.com>
2023-10-14 10:29:09 +0900

    :sparkles: Feat: Reusable을 확장.
    Reusable 프로토콜을 확장하여 직접적인 구현부 로직을 구현하였습니다.

--- GithubFollower/GithubFollower/Extensions/Reusable+.swift
+++ GithubFollower/GithubFollower/Extensions/Reusable+.swift
@@ -5,4 +5,10 @@
 //  Created by Minseong Kang on 10/14/23.
 //
 
-import Foundation
+import UIKit
+
+extension Reusable where Self: UIView {
+    static var reuseIdentifier: String {
+        return String(describing: self)
+    }
+}
