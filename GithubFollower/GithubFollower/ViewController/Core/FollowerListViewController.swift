7a5cfe30be82c3cff327e696eaa66666ff25c602
Minseong Kang <dev.skyachieve91@gmail.com>
2023-10-14 14:13:30 +0900

    :recycle: Refactor: CollectionView 테스트를 위한 기본 구현.
    CollectionView의 UI 확인을 위한 기본적인 UI들을 구현했습니다.

--- GithubFollower/GithubFollower/ViewController/Core/FollowerListViewController.swift
+++ GithubFollower/GithubFollower/ViewController/Core/FollowerListViewController.swift
@@ -12,6 +12,22 @@ final class FollowerListViewController: UIViewController {
     var username: String
     let networkManager: NetworkManagerProtocol
     
+    lazy var collectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        
+        var collectionView = UICollectionView(
+            frame: self.view.bounds,
+            collectionViewLayout: flowLayout)
+        
+        collectionView.backgroundColor = .systemPink
+        collectionView.register(
+            FollowerCell.self,
+            forCellWithReuseIdentifier: FollowerCell.reuseIdentifier
+        )
+        
+        return collectionView
+    }()
+    
     init(networkManager: NetworkManagerProtocol, username: String) {
         self.networkManager = networkManager
         self.username = username
@@ -26,36 +42,46 @@ final class FollowerListViewController: UIViewController {
         super.viewDidLoad()
         
         initialSetup()
+        configureCollectionView()
+        getFollower()
         
-        // MARK: - TEST
-        networkManager.getFollower(
-            username: username,
-            perPage: 100,
-            page: 1) { result in
-                switch result {
-                case .success(let followers):
-                    print(followers)
-                case .failure(let error):
-                    self.presentGFAlertOnMainThread(
-                        alertTitle: "Bad Stuff Happend",
-                        message: error.localizedDescription,
-                        buttonTitle: "OK"
-                    )
-                }
-            }
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(false, animated: true)
-        
-     
-        
-        
     }
+}
+
+extension FollowerListViewController {
     
     private func initialSetup() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func configureCollectionView() {
+        view.addSubview(collectionView)
+    }
+    
+    private func getFollower() {
+        // MARK: - TEST
+        networkManager.getFollower(
+            username: username,
+            perPage: 100,
+            page: 1
+        ) { result in
+            switch result {
+            case .success(let followers):
+                print(followers)
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(
+                    alertTitle: "Bad Stuff Happend",
+                    message: error.localizedDescription,
+                    buttonTitle: "OK"
+                )
+            }
+        }
     }
 }
