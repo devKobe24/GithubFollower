75b023e1830738cb4df9550ac85e0bc541ad9c2c
Kobe <dev.skyachieve91@gmail.com>
2023-10-14 16:25:25 +0900

    :sparkles: Feat: UICollectionViewFlowLayout 초기 구현.
    UICollectionViewFlowLayout을 함수로 구현하였습니다.

--- GithubFollower/GithubFollower/ViewController/Core/FollowerListViewController.swift
+++ GithubFollower/GithubFollower/ViewController/Core/FollowerListViewController.swift
@@ -13,7 +13,7 @@ final class FollowerListViewController: UIViewController {
     let networkManager: NetworkManagerProtocol
     
     lazy var collectionView: UICollectionView = {
-        let flowLayout = UICollectionViewFlowLayout()
+        let flowLayout = configureFlowLayout()
         
         var collectionView = UICollectionView(
             frame: self.view.bounds,
@@ -63,6 +63,29 @@ extension FollowerListViewController {
     
     private func configureCollectionView() {
         view.addSubview(collectionView)
+    }
+    
+    private func configureFlowLayout() -> UICollectionViewFlowLayout {
+        let widthOfView: CGFloat = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth: CGFloat = widthOfView - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth: CGFloat = availableWidth / 3
+        let itemHeight: CGFloat = availableWidth + 40
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(
+            top: padding,
+            left: padding,
+            bottom: padding,
+            right: padding
+        )
+        flowLayout.itemSize = CGSize(
+            width: itemWidth,
+            height: itemHeight
+        )
+        
+        return flowLayout
     }
     
     private func getFollower() {
