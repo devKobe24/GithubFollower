323a6a7d18bd6558f6dc6d7ba5103145a57f15df
Kobe <dev.skyachieve91@gmail.com>
2023-10-16 06:08:13 +0900

    :recycle: Chore: AssetImage case 추가.
    AssetImage의 case를 추가하였습니다.

--- GithubFollower/GithubFollower/Model/Enum/AssetImages.swift
+++ GithubFollower/GithubFollower/Model/Enum/AssetImages.swift
@@ -7,11 +7,17 @@
 
 enum AssetImages {
     case ghLogoImage
+    case ghAvatarPlaceHolder
+    case ghEmptyStateLogo
     
     var localized: String {
         switch self {
         case .ghLogoImage:
             return "gh-logo"
+        case .ghAvatarPlaceHolder:
+            return "avatar-placeholder"
+        case .ghEmptyStateLogo:
+            return "empty-state-logo"
         }
     }
 }
