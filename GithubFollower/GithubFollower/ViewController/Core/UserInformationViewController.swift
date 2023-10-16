f2fcc49dbdff6ea853d67f611dc92fc2e685a8d0
Kobe <dev.skyachieve91@gmail.com>
2023-10-17 08:38:09 +0900

    :recycle: Refactor: 유저 헤더뷰 적용, container view 생성.
    유저의 정보를 보여주는 헤더 뷰를 적용하였으며 github의 repo를 보여줄 container 뷰와 gitgist를 보여줄 container 뷰를 생성 및 구현하였습니다.

--- GithubFollower/GithubFollower/ViewController/Core/UserInformationViewController.swift
+++ GithubFollower/GithubFollower/ViewController/Core/UserInformationViewController.swift
@@ -9,8 +9,14 @@ import UIKit
 
 final class UserInformationViewController: UIViewController {
     
+    let headerView: UIView = UIView()
+    let githubRepoView: UIView = UIView()
+    let gitgistView: UIView = UIView()
+    
     var networkManager: NetworkManager
     var username: String?
+    
+    let padding: CGFloat = 20
     
     init(username: String?, networkManager: NetworkManager) {
         self.networkManager = networkManager
@@ -28,6 +34,16 @@ final class UserInformationViewController: UIViewController {
         
         initialSetup()
         configureDoneButton()
+        
+        configureHeaderView()
+        constraintsHeaderView()
+        
+        configureGithubRepoView()
+        constraintsGithubRepoView()
+        
+        configureGitgistView()
+        constraintsGitgistView()
+        
         guard let username = username else { return }
         networkManager.getUserInfo(
             username: username
@@ -36,11 +52,81 @@ final class UserInformationViewController: UIViewController {
             
             switch result {
             case .success(let userData):
-                print("USER DATA ==> \(userData)")
+                DispatchQueue.main.async {
+                    self.add(
+                        childViewController: GFUserInfoHeaderViewController(
+                            user: userData
+                        ),
+                        to: self.headerView
+                    )
+                }
             case .failure(let error):
                 self.presentGFAlertOnMainThread(alertTitle: "Error!!", message: error.localizedDescription, buttonTitle: "OK.")
             }
         }
+    }
+}
+
+extension UserInformationViewController {
+    private func configureHeaderView() {
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(headerView)
+        
+        headerView.backgroundColor = .systemBackground
+    }
+    
+    private func constraintsHeaderView() {
+        let height = (view.frame.height) - (view.frame.height - 180)
+        
+        NSLayoutConstraint.activate([
+            headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            headerView.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
+    
+    private func configureGithubRepoView() {
+        githubRepoView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(githubRepoView)
+        
+        githubRepoView.backgroundColor = .systemPink
+    }
+    
+    private func constraintsGithubRepoView() {
+        let height = (view.bounds.height) - (view.bounds.height - 140)
+        
+        NSLayoutConstraint.activate([
+            githubRepoView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+            githubRepoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            githubRepoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            githubRepoView.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
+    
+    private func configureGitgistView() {
+        gitgistView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(gitgistView)
+        
+        gitgistView.backgroundColor = .systemBlue
+    }
+    
+    private func constraintsGitgistView() {
+        let height = (view.bounds.height) - (view.bounds.height - 140)
+        
+        NSLayoutConstraint.activate([
+            gitgistView.topAnchor.constraint(equalTo: githubRepoView.bottomAnchor, constant: padding),
+            gitgistView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            gitgistView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            gitgistView.heightAnchor.constraint(equalToConstant: height)
+        ])
+    }
+    
+    private func add(childViewController: UIViewController, to containerView: UIView) {
+        addChild(childViewController)
+        containerView.addSubview(childViewController.view)
+        childViewController.view.frame = containerView.bounds
+        childViewController.didMove(toParent: self)
     }
 }
 
