43f1cb8c532cbfa91e9767a7a4815a9bdbdd1c14
Kobe <dev.skyachieve91@gmail.com>
2023-10-17 09:49:51 +0900

    :recycle: Chore: SFSymbol Case 추가.
    SFSymbol case를 추가하였습니다.

--- GithubFollower/GithubFollower/Model/Enum/SFSymbols.swift
+++ GithubFollower/GithubFollower/Model/Enum/SFSymbols.swift
@@ -7,11 +7,23 @@
 
 enum SFSymbols {
     case mappinAndEllipse
+    case folder
+    case textAlignleft
+    case heart
+    case personTwo
     
     var imageName: String {
         switch self {
         case .mappinAndEllipse:
             return "mappin.and.ellipse"
+        case .folder:
+            return "folder"
+        case .textAlignleft:
+            return "text.alignleft"
+        case .heart:
+            return "heart"
+        case .personTwo:
+            return "person.2"
         }
     }
 }
